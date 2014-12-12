c579025afa57b9337b5b87fcb9c16ca27d3b9348
Aaron Jacobs <atheriel@gmail.com>
2014-12-11 20:12:56 -0500

    Stub out a basic type structure for L-Systems.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+pub struct Rule<T> {
+    predecessor: Vec<T>,
+    successor: Vec<T>
+}
+
+pub struct LSystem<T> {
+    alphabet: Vec<T>,
+    axiom: T,
+    rules: Vec<Rule<T>>
+}
+
 #[test]
 fn it_works() {
 }
