dff31497fd0d781e13a35f3cc530b2bcf4817822
Aaron Jacobs <atheriel@gmail.com>
2014-12-12 02:00:58 -0500

    Implements a much nicer Algae definition and printing.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/algae.rs
+++ src/algae.rs
@@ -2,31 +2,33 @@ extern crate lsystem;
 
 use lsystem::LSystemType;
 
-#[deriving(Clone)]
+#[deriving(Show, Clone)]
 pub enum AlgeaAlphabet {
-	AlgaeA,
-	AlgaeB,
-}
-
-impl std::fmt::Show for AlgeaAlphabet {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            &AlgeaAlphabet::AlgaeA => write!(f, "{}", "A"),
-            &AlgeaAlphabet::AlgaeB => write!(f, "{}", "B")
-        }
-    }
+    A,
+    B,
 }
 
 fn algae_rule(input: AlgeaAlphabet) -> Vec<AlgeaAlphabet> {
-	match input {
-		AlgeaAlphabet::AlgaeA => vec![AlgeaAlphabet::AlgaeA, AlgeaAlphabet::AlgaeB],
-		AlgeaAlphabet::AlgaeB => vec![AlgeaAlphabet::AlgaeA]
-	}
+    match input {
+        AlgeaAlphabet::A => vec![AlgeaAlphabet::A, AlgeaAlphabet::B],
+        AlgeaAlphabet::B => vec![AlgeaAlphabet::A]
+    }
 }
 
 fn main() {
-	let algae_lsystem = LSystemType::from_fn(algae_rule);
-	for n in algae_lsystem.recurse(AlgeaAlphabet::AlgaeA).take(5) {
-		println!("{}", n)
-	}
+    let algae_lsystem = LSystemType::from_fn(algae_rule);
+    let algae_n7 = algae_lsystem.recurse(AlgeaAlphabet::A).take(8);
+
+    // Print out the first eight levels of the Algae sequence in the same
+    // format as in the Wikipedia article.
+    for (index, n) in algae_n7.enumerate() {
+        let mut printed = format!("n = {}: ", index);
+        for i in n.iter() {
+            match i {
+                &AlgeaAlphabet::A => printed.push_str("A"),
+                &AlgeaAlphabet::B => printed.push_str("B")
+            }
+        }
+        println!("{}", printed)
+    }
 }
