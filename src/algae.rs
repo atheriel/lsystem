5dc564b22401b2c121a2d0dc2711556a24d0b2e2
Aaron Jacobs <atheriel@gmail.com>
2014-12-16 21:47:18 -0500

    Updates the Algae example to use the new iterator function.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/algae.rs
+++ src/algae.rs
@@ -1,32 +1,30 @@
 extern crate lsystem;
 
-use lsystem::LSystemType;
+use lsystem::lsystem_iter;
 
-#[deriving(Show, Clone)]
-pub enum AlgeaAlphabet {
-    A,
-    B,
+#[deriving(Clone)]
+pub enum AlgeaState {
+    Reproduction,
+    Growth,
 }
 
-fn algae_rule(input: AlgeaAlphabet) -> Vec<AlgeaAlphabet> {
+fn algae_rule(input: AlgeaState) -> Vec<AlgeaState> {
     match input {
-        AlgeaAlphabet::A => vec![AlgeaAlphabet::A, AlgeaAlphabet::B],
-        AlgeaAlphabet::B => vec![AlgeaAlphabet::A]
+        AlgeaState::Reproduction => vec![AlgeaState::Reproduction, AlgeaState::Growth],
+        AlgeaState::Growth => vec![AlgeaState::Reproduction]
     }
 }
 
 fn main() {
-    let algae_lsystem = LSystemType::from_fn(algae_rule);
-    let algae_n7 = algae_lsystem.recurse(AlgeaAlphabet::A).take(8);
-
     // Print out the first eight levels of the Algae sequence in the same
     // format as in the Wikipedia article.
-    for (index, n) in algae_n7.enumerate() {
+    for (index, n) in lsystem_iter(AlgeaState::Growth, algae_rule).
+                      take(8).enumerate() {
         let mut printed = format!("n = {}: ", index);
         for i in n.iter() {
             match i {
-                &AlgeaAlphabet::A => printed.push_str("A"),
-                &AlgeaAlphabet::B => printed.push_str("B")
+                &AlgeaState::Reproduction => printed.push_str("A"),
+                &AlgeaState::Growth => printed.push_str("B")
             }
         }
         println!("{}", printed)
