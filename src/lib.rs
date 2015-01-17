eb201ce327606cfe6aba873c8c63778908b2fd98
Aaron Jacobs <atheriel@gmail.com>
2015-01-17 18:15:13 -0500

    Improves documentation, and moves to the recommended linewidth of 100.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -64,53 +64,47 @@
 //!            vec!(Algae::A, Algae::B, Algae::A, Algae::A, Algae::B))
 //! ```
 
-/// Create the Lindenmayer System defined by an axiom of type `Vec<T>`, a rule
-/// function which maps values of type `T` to vectors of values of type `T`,
-/// and the set of all possible values of type `T`.
+/// Create the Lindenmayer System defined by an axiom of type `Vec<T>`, a rule function (or
+/// closure) which maps values of type `T` to vectors of values of type `T`, and the set of all
+/// possible values of type `T`.
 ///
-/// Formally, an [L-System](http://en.wikipedia.org/wiki/l_system) consists of
-/// three things:
+/// Formally, an [L-system](http://en.wikipedia.org/wiki/l_system) consists of three things:
 ///
 /// 1. An alphabet of letters
 /// 2. An axiom composed of letters of this alphabet; and
-/// 3. A set of "production" rules for transforming sets of letters into one
-///    another.
+/// 3. A set of "production" rules for transforming sets of letters into one another.
 ///
-/// This definition satisfies these requirements by taking the universe of the
-/// values of type `T` as the alphabet, one specific value of type `T` as the
-/// axiom, and a function `T -> Vec<T>` as handling any transformations. This
-/// is really just a way of using Rust's type system to express a formal
-/// grammar in a very concise way. And while there's no reason one could not
-/// use regular types (like `int` or `&str`) here, this method really comes
-/// into its own through the use of `enum`s.
+/// This definition satisfies these requirements by taking the universe of the values of type `T`
+/// as the alphabet, one specific vector of values of type `T` as the axiom, and a function or
+/// closure of type `FnMut(T) -> Vec<T>` for handling any transformations. This is really just a
+/// way of using Rust's type system to express a formal grammar in a very concise way. And while
+/// there's no reason one could not use regular types (like `int` or `&str`) here, this method
+/// really comes into its own through the use of `enum`s.
 ///
 /// ## Iterating over the L-System
 ///
-/// The [`iter()`](#method.iter) method returns a Rust iterator that yields
-/// successive iterations of the L-System. This allows very idiomatic handling
-/// of iteration, but be warned: the iterator will never be exhausted, so any
-/// loops must be broken manually.
+/// The [`iter()`](#method.iter) method returns a Rust iterator that yields successive iterations
+/// of the L-System. This allows very idiomatic handling of iteration, but be warned: the iterator
+/// will never be exhausted, so any loops must be broken manually.
 pub struct LSystemType<T, F: FnMut(T) -> Vec<T>> {
     axiom: Vec<T>,
     rules: F
 }
 
 impl<T, F> LSystemType<T, F> where F: FnMut(T) -> Vec<T> {
+    /// Creates a new representation of an L-system with the given axiom and production rules.
     pub fn new(axiom: Vec<T>, rules: F) -> LSystemType<T, F> {
         LSystemType { axiom: axiom, rules: rules }
     }
 
+    /// Consumes the `LSystemType` to produce an iterator.
     pub fn iter(self) -> LSystemIterator<T, F> {
-        LSystemIterator {
-            current_state: self.axiom,
-            rules: self.rules,
-            zeroth: true
-        }
+        LSystemIterator { current_state: self.axiom, rules: self.rules, zeroth: true }
     }
 }
 
-/// Defines an iterator over an L-System, where each successive iteration
-/// applies a series of rules to the current axiom to produce a new axiom.
+/// Defines an iterator over an L-System, where each successive iteration applies a series of rules
+/// to the current axiom to produce a new axiom.
 pub struct LSystemIterator<T, F: FnMut(T) -> Vec<T>> {
     current_state: Vec<T>,
     rules: F,
@@ -121,15 +115,15 @@ impl<T, F> Iterator for LSystemIterator<T, F> where T: Clone, F: FnMut(T) -> Vec
     type Item = Vec<T>;
 
     fn next(&mut self) -> Option<Vec<T>> {
-        // In order to ensure that the "n = 0" case returns the original axiom,
-        // store whether we are in this state or not.
+        // In order to ensure that the "n = 0" case returns the original axiom, store whether we
+        // are in this state or not.
         if self.zeroth {
             self.zeroth = false;
             return Some(self.current_state.clone())
         }
 
-        // Otherwise, apply the production rules to the axiom to produce a new
-        // axiom for the iteration level.
+        // Otherwise, apply the production rules to the axiom to produce a new axiom for the
+        // iteration level.
         let mut new_state: Vec<T> = Vec::new();
         for element in self.current_state.iter().cloned() {
             let entry = (self.rules)(element);
